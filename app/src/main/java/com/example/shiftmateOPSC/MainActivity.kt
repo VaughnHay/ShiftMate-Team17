75dcbf8ddec4a7f3ffee8000ee5eee4e351decf7
LeboGP <104358403+LeboGP@users.noreply.github.com>
2024-05-03 02:59:54 +0200

    Updated view page done by Lebo_GP

--- app/src/main/java/com/example/shiftmateOPSC/MainActivity.kt
+++ app/src/main/java/com/example/shiftmateOPSC/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.shiftmateOPSC
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+/*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_layout)
     }
-}
+
+} */
