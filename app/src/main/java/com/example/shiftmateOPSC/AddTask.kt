5264809a2eee9be797cdda59e463368e80671903
ryanquinn <st10135847@vcconnect.edu.za>
2024-05-03 10:44:13 +0200

    Changed time selection on the add task page to make use of timepickers

--- app/src/main/java/com/example/shiftmateOPSC/AddTask.kt
+++ app/src/main/java/com/example/shiftmateOPSC/AddTask.kt
@@ -1,19 +1,14 @@
 package com.example.shiftmateOPSC
 
 import android.Manifest
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
-import android.widget.AutoCompleteTextView
-import android.widget.Button
-import android.widget.CalendarView
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -21,6 +16,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import java.io.ByteArrayOutputStream
+import java.util.*
 
 class AddTask : AppCompatActivity() {
 
@@ -29,9 +25,9 @@ class AddTask : AppCompatActivity() {
     private lateinit var firebaseStorage: FirebaseStorage
     private lateinit var firebaseDatabase: FirebaseDatabase
 
-    private lateinit var linearLayoutTasks: LinearLayout
     private lateinit var startTimeEditText: EditText
     private lateinit var endTimeEditText: EditText
+
     private lateinit var descriptionEditText: EditText
     private lateinit var categoryAutoCompleteTextView: AutoCompleteTextView
     private lateinit var saveButton: Button
@@ -54,10 +50,11 @@ class AddTask : AppCompatActivity() {
         firebaseStorage = FirebaseStorage.getInstance()
         firebaseDatabase = FirebaseDatabase.getInstance()
 
-
         // Initialize views
         startTimeEditText = findViewById(R.id.StartTimeTV)
         endTimeEditText = findViewById(R.id.EndTimeTV)
+        startTimeEditText.isFocusable = false
+        endTimeEditText.isFocusable = false
         descriptionEditText = findViewById(R.id.DescriptionTV)
         categoryAutoCompleteTextView = findViewById(R.id.CategoryACTV)
         saveButton = findViewById(R.id.btnSave)
@@ -93,10 +90,6 @@ class AddTask : AppCompatActivity() {
             }
         }
 
-        backButton.setOnClickListener{
-            finish()
-        }
-
         // Set onClickListener for the take picture button
         takePictureButton.setOnClickListener {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -111,104 +104,137 @@ class AddTask : AppCompatActivity() {
             // Store the selected date in the format you desire
             dateSelected = "$year-${month + 1}-$dayOfMonth"
         }
-    }
-                private fun openCamera() {
-                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                if (cameraIntent.resolveActivity(packageManager) != null) {
-                    startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
-                } else {
-                    // Notify the user that the camera app is not available
-                    Toast.makeText(this, "Camera app is not available", Toast.LENGTH_SHORT).show()
-                }
-            }
 
-            override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-                super.onActivityResult(requestCode, resultCode, data)
-                if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
-                    val extras = data?.extras
-                    imageBitmap = extras?.get("data") as Bitmap
-                    imageView.setImageBitmap(imageBitmap)
-                    imageView.visibility = View.VISIBLE
-                }
-            }
-
-            override fun onRequestPermissionsResult(
-                requestCode: Int,
-                permissions: Array<out String>,
-                grantResults: IntArray
-            ) {
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-                if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
-                    if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        openCamera()
-                    }
-                }
-            }
-
-            private fun uploadImageToFirebaseStorage(
-                bitmap: Bitmap,
-                startTime: String,
-                endTime: String,
-                description: String,
-                category: String,
-                date: String
-            ) {
-                val currentUser = firebaseAuth.currentUser
-                val storageReference =
-                    firebaseStorage.reference.child("images/${currentUser?.uid}/${System.currentTimeMillis()}.jpg")
-
-                val baos = ByteArrayOutputStream()
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-                val imageData = baos.toByteArray()
-
-                val uploadTask = storageReference.putBytes(imageData)
-                uploadTask.addOnSuccessListener { taskSnapshot ->
-                    storageReference.downloadUrl.addOnSuccessListener { uri ->
-                        val imageUrl = uri.toString()
-                        saveTaskToFirebaseDatabase(
-                            imageUrl,
-                            startTime,
-                            endTime,
-                            description,
-                            category,
-                            date
-                        )
-                    }
-                }.addOnFailureListener { exception ->
-                    // Handle error
-                }
-            }
-
-            private fun saveTaskToFirebaseDatabase(
-                imageUrl: String?,
-                startTime: String,
-                endTime: String,
-                description: String,
-                category: String,
-                date: String
-            ) {
-                val currentUser = firebaseAuth.currentUser
-                val userUid = currentUser?.uid ?: return  // Return if user is null
-
-                val tasksReference = firebaseDatabase.reference.child("TimeLog").child(userUid)
-
-                val taskMap = HashMap<String, Any>()
-                taskMap["startTime"] = startTime
-                taskMap["endTime"] = endTime
-                taskMap["description"] = description
-                taskMap["category"] = category
-                taskMap["date"] = date
-                imageUrl?.let { taskMap["imageUrl"] = it }
-
-                val taskId = tasksReference.push().key ?: ""
-                tasksReference.child(taskId).setValue(taskMap)
-                    .addOnSuccessListener {
-                        // Task saved successfully
-                        // You can add any further action here if needed
-                    }.addOnFailureListener { exception ->
-                        // Handle error
-                    }
-            }
+        // Set OnClickListeners for start and end time EditTexts
+        startTimeEditText.setOnClickListener {
+            showTimePickerDialog(startTimeEditText)
         }
 
+        endTimeEditText.setOnClickListener {
+            showTimePickerDialog(endTimeEditText)
+        }
 
+        // Set onClickListener for the back button
+        backButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    // Function to show TimePickerDialog
+    private fun showTimePickerDialog(editText: EditText) {
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+
+        val timePickerDialog = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener { _, selectedHour, selectedMinute ->
+                val selectedTime = String.format("%02d:%02d", selectedHour, selectedMinute)
+                editText.setText(selectedTime)
+            },
+            hour,
+            minute,
+            true // Set this to true to show minutes
+        )
+
+        timePickerDialog.show()
+    }
+
+    private fun openCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (cameraIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
+        } else {
+            // Notify the user that the camera app is not available
+            Toast.makeText(this, "Camera app is not available", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            val extras = data?.extras
+            imageBitmap = extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+            imageView.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera()
+            }
+        }
+    }
+
+    private fun uploadImageToFirebaseStorage(
+        bitmap: Bitmap,
+        startTime: String,
+        endTime: String,
+        description: String,
+        category: String,
+        date: String
+    ) {
+        val currentUser = firebaseAuth.currentUser
+        val storageReference =
+            firebaseStorage.reference.child("images/${currentUser?.uid}/${System.currentTimeMillis()}.jpg")
+
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val imageData = baos.toByteArray()
+
+        val uploadTask = storageReference.putBytes(imageData)
+        uploadTask.addOnSuccessListener { taskSnapshot ->
+            storageReference.downloadUrl.addOnSuccessListener { uri ->
+                val imageUrl = uri.toString()
+                saveTaskToFirebaseDatabase(
+                    imageUrl,
+                    startTime,
+                    endTime,
+                    description,
+                    category,
+                    date
+                )
+            }
+        }.addOnFailureListener { exception ->
+            // Handle error
+        }
+    }
+
+    private fun saveTaskToFirebaseDatabase(
+        imageUrl: String?,
+        startTime: String,
+        endTime: String,
+        description: String,
+        category: String,
+        date: String
+    ) {
+        val currentUser = firebaseAuth.currentUser
+        val userUid = currentUser?.uid ?: return  // Return if user is null
+
+        val tasksReference = firebaseDatabase.reference.child("TimeLog").child(userUid)
+
+        val taskMap = HashMap<String, Any>()
+        taskMap["startTime"] = startTime
+        taskMap["endTime"] = endTime
+        taskMap["description"] = description
+        taskMap["category"] = category
+        taskMap["date"] = date
+        imageUrl?.let { taskMap["imageUrl"] = it }
+
+        val taskId = tasksReference.push().key ?: ""
+        tasksReference.child(taskId).setValue(taskMap)
+            .addOnSuccessListener {
+                // Task saved successfully
+                // You can add any further action here if needed
+            }.addOnFailureListener { exception ->
+                // Handle error
+            }
+    }
+}
